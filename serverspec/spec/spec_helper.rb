7208bc1201fe819cf883971de2325ab83ed9b6b8
yuhei1012 <102453302+yuhei1012@users.noreply.github.com>
2024-09-26 17:50:49 +0900

    Update spec_helper.rb

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -1,10 +1,19 @@
 require 'serverspec'
-set :backend, :ssh
+require 'net/ssh'
 
-# SSHの接続情報を設定します
-host = ENV['TARGET_HOST']
+# ターゲットホストにSSHで接続するための設定
+host = 'TARGET_EC2_IP_ADDRESS' # ここにターゲットEC2のIPアドレスを指定
+user = 'ec2-user'  # 接続に使うユーザー名
+key  = '~/.ssh/your_private_key.pem'  # 秘密鍵のパスを指定
+
+# SSHの設定
 options = Net::SSH::Config.for(host)
-options[:user] = 'ec2-user'  # SSH接続するユーザー名
+options[:user] = user
+options[:keys] = [key]
+options[:port] = 22  # デフォルトのSSHポート
 
-set :host,        options[:host_name] || host
+# ServerspecのバックエンドをSSHに設定
+set :backend, :ssh
+set :host, host
 set :ssh_options, options
+
