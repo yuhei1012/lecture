a5cefc83819e07147cbf922ac43eb80e14beba5b
yuhei1012 <102453302+yuhei1012@users.noreply.github.com>
2024-09-28 01:07:37 +0900

    Update spec_helper.rb

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -5,12 +5,15 @@ require 'net/ssh'
 set :backend, :ssh
 
 # EC2インスタンスの情報を設定
+host = ENV['EC2_PUBLIC_IP']  # 環境変数からEC2のパブリックIPを取得
+user = 'ec2-user'            # EC2のデフォルトユーザー
+
 options = {
-  user: ec2-user,
-  keys: [ENV[SSH_KEY_PATH]],   # 環境変数からSSHキーのパスを取得
+  user: user,
+  keys: [ENV['SSH_KEY_PATH']],   # 環境変数からSSHキーのパスを取得
   keys_only: true,
   auth_methods: ['publickey'],
-  verify_host_key: :never    # ホストキーの検証は無効化
+  verify_host_key: :never    # ホストキーの検証を無効化
 }
 
 set :host, host
