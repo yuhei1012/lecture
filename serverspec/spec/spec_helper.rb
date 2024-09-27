82ee37d6df6ae7ed1c55f86d5b974ca1e1d1e6c8
yuhei1012 <102453302+yuhei1012@users.noreply.github.com>
2024-09-28 00:53:40 +0900

    Update spec_helper.rb

--- serverspec/spec/spec_helper.rb
+++ serverspec/spec/spec_helper.rb
@@ -5,18 +5,14 @@ require 'net/ssh'
 set :backend, :ssh
 
 # EC2インスタンスの情報を設定
-host = ENV['EC2_PUBLIC_IP']  # 環境変数からEC2のパブリックIPを取得
-user = 'ec2-user'            # デフォルトユーザー
-
 options = {
-  user: user,
-  keys: ['~/.ssh/id_rsa'],   # CircleCIが自動で設定するSSHキーのパス
+  user: ec2-user,
+  keys: [ENV[SSH_KEY_PATH]],   # 環境変数からSSHキーのパスを取得
   keys_only: true,
   auth_methods: ['publickey'],
-  #verify_host_key: :never    # ホストキーの検証を無効化
+  verify_host_key: :never    # ホストキーの検証は無効化
 }
 
 set :host, host
 set :ssh_options, options
-
 
